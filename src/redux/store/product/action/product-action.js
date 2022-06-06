f05de475d65f1fd1293e04db1c281e3f39de7a0b
hasibulHossain <hasibulhossain52@gmail.com>
2022-06-06 20:10:52 +0600

    added docs on actions file

--- src/redux/store/product/action/product-action.js
+++ src/redux/store/product/action/product-action.js
@@ -1,37 +1,24 @@
 import axios from 'axios';
+import { variantNameEnum } from '../../../../common/enum';
 import * as types from '../types';
 
 /**
- * a summery
- * @param {String} test 
- * @returns {{type: Number, payload: String}}
+ * Fetch product details from remote url
+ * @since V 1.0.0
+ * @returns {void}
  */
-// export const testOne = (test) => {
-//     return {
-//         type: types.PRODUCT_LOADING,
-//         payload: test
-//     }
-// };
-
-
-const propEnum = {
-    "Color": "Color",
-    "Shoe Size": "Shoe Size"
-  }
-  
-
-
-// * @return void Dispatch `GET_SINGLE_PRODUCT_DETAILS`
-// */
 export const fetchProductDetails = () => async (dispatch) => {
+    const baseUrl = "https://moveon-api-server.sbox.ali2bd.net/api/v1";
+    const url = `${baseUrl}/customer/dummy-product`;
+
    try {
-       const url = "https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product";
 
        dispatch({type: types.PRODUCT_LOADING_INPROGRESS, payload: {loading: true}});
 
        const res = await axios.get(url);
 
-       const colorVariations = res.data.variation.props.filter(item => item.name === propEnum.Color);
+        // rename "image" property name with "url" property on color variant value as per need
+       const colorVariations = res.data.variation.props.filter(item => item.name === variantNameEnum['Color']);
        const updatedColorVariations = colorVariations[0].values.map(item => {
            return {
                ...item,
@@ -64,7 +51,12 @@ export const fetchProductDetails = () => async (dispatch) => {
 
 
 
-
+/**
+ * 
+ * @since V 1.0.0
+ * @param {{isVariantColorSelected: boolean, swiper: object, selectedVariantColor: object}}  
+ * @returns {void} - Dispatch PRODUCT_SLIDER_UPDATED 
+ */
 export const toggleIsVariantSelected = ({isVariantColorSelected: isVariantColorSelected, swiper: swiper, selectedVariantColor: selectedVariantColor}) => {
     const payload = {
         selectedVariantColor: selectedVariantColor,
@@ -74,17 +66,27 @@ export const toggleIsVariantSelected = ({isVariantColorSelected: isVariantColorS
 
     return { 
         type: types.PRODUCT_SLIDER_UPDATED,
-        payload: payload };
+        payload: payload
+    };
 }
 
-
-export const updateSliderArr = (gallery) => {
+/**
+ * @since V 1.0.0
+ * @param {array} gallery - slider image array
+ * @returns {void} - dispatch PRODUCT_SLIDER_GALLERY_UPDATED
+ */
+export const updateSliderArr = (galleryImages) => {
     return {
         type: types.PRODUCT_SLIDER_GALLERY_UPDATED,
-        payload: gallery
+        payload: galleryImages
     }
 }
 
+/**
+ * @since V 1.0.0
+ * @param {object} product - updated product
+ * @returns {void} - dispatch PRODUCT_UPDATE
+ */
 export const updateProduct = (product) => {
     const payload = {
         discountedPrice: null,
@@ -104,6 +106,12 @@ export const updateProduct = (product) => {
     }
 }
 
+/**
+ * Calculate value between old and discounted price and return discount on percent.
+ * @param {Number} discountPrice - $80
+ * @param {Number} oldPrice - $100
+ * @returns {Number} - will return 20 for this example
+ */
 function calculateDiscountPercentage( discountPrice, oldPrice) {
     const discount_percent = 100 - ( ( discountPrice / oldPrice ) * 100 );
     const discount = '' +  Math.round( discount_percent);
--- src/redux/store/product/reducer/product-reducer.js
+++ src/redux/store/product/reducer/product-reducer.js
@@ -1,5 +1,32 @@
 import * as actions from '../types';
-  
+
+const initialState = {
+  isLoading: false,
+  isFetched: false,
+  error: false,
+  id: null,
+  title: "Product title",
+  discountedPrice: 25,
+  oldPrice: 30,
+  discountedPercentage: 30,
+  productDetails: {
+      mainImage: "",
+      gallery: [],
+      ratingCount: null,
+      ratingAverage: null,
+      skus: [],
+      variants: []
+  },
+  ui: {
+      swiper: null,
+      isVariantColorSelected: false,
+      selectedVariantColor: {
+        color: [],
+        size: []
+      },
+  }
+};
+
 function product(state = initialState, { type, payload }) {
     switch (type) {
         case actions.PRODUCT_LOADING_INPROGRESS:
@@ -67,164 +94,5 @@ function product(state = initialState, { type, payload }) {
         return state;
     }
 };
-
-
-let mockData = {
-    skus: [
-        {
-            "id": 12000028392067142,
-            "price": { "discounted": 21.41, "old": 38.22 },
-            "props": [691, 100010487],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067140,
-            "price": { "discounted": 22.41, "old": 58.22 },
-            "props": [691, 200000337],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067141,
-            "price": { "discounted": 24.41, "old": 38.22 },
-            "props": [691, 200000338],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067138,
-            "price": { "discounted": 21.47, "old": 38.72 },
-            "props": [691, 100013888],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067139,
-            "price": { "discounted": 29.41, "old": 39.22 },
-            "props": [691, 100010483],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067136,
-            "price": { "discounted": 24.41, "old": 33.22 },
-            "props": [771, 100010487],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067137,
-            "price": { "discounted": 22.41, "old": 38.28 },
-            "props": [691, 200000364],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067134,
-            "price": { "discounted": 26.41, "old": 54.22 },
-            "props": [771, 200000337],
-            "stock": 290
-          },
-          {
-            "id": 12000028392067135,
-            "price": { "discounted": 23.41, "old": 57.22 },
-            "props": [771, 200000338],
-            "stock": 290
-          },
-    ],
-    gallery: [
-        {
-          url: "https://ae01.alicdn.com/kf/S7e556daed6ec45e5b605cc8a024badf0d/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg",
-          thumb: "https://ae01.alicdn.com/kf/S7e556daed6ec45e5b605cc8a024badf0d/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_50x50.jpg",
-          title: null
-        },
-        {
-          url: "https://ae01.alicdn.com/kf/Sf162208134bd4f7ca98013e1beb50b80J/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg",
-          thumb: "https://ae01.alicdn.com/kf/Sf162208134bd4f7ca98013e1beb50b80J/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_50x50.jpg",
-          title: null
-        },
-        {
-          url: "https://ae01.alicdn.com/kf/S61db37a0f0f445c58a2ea47c523bbe7cW/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg",
-          thumb: "https://ae01.alicdn.com/kf/S61db37a0f0f445c58a2ea47c523bbe7cW/ 2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_50x50.jpg",
-          title: null
-        }
-      ],
-      props: [{
-        id: 14,
-        name: "Color",
-        values: [
-          {
-            id: 771,
-            name: "Beige",
-            color: "#F8F7E7",
-            url: "https://ae01.alicdn.com/kf/S5d242b090abb48299274f75f0822b703i/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_640x640.jpg",
-            thumb: "https://ae01.alicdn.com/kf/S5d242b090abb48299274f75f0822b703i/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_50x50.jpg",
-            title: "Black"
-          },
-          {
-            id: 691,
-            name: "Gray",
-            color: "#999",
-            url: "https://ae01.alicdn.com/kf/S6f7e4e8aecbd4f34bba78b9319e7b05ao/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_640x640.jpg",
-            thumb: "https://ae01.alicdn.com/kf/S6f7e4e8aecbd4f34bba78b9319e7b05ao/2022-New-Men-s-Shoes-Mesh-Sneakers-Gray-Tennis-Shoes-Moccasin-Shoes-Lace-Up-Loafers-Comfortable.jpg_50x50.jpg",
-            title: "Gray"
-          }
-        ]
-      },
-      {
-        id:200000124,
-        name:"Shoe Size",
-        values:[
-        {
-        id:200000364,
-        name:"39",
-        color:null,
-        image:null,
-        thumb:null,
-        title:"39"
-        },
-        {
-        id:100013888,
-        name:"40",
-        color:null,
-        image:null,
-        thumb:null,
-        title:"40"
-        },
-        {
-        id:100010483,
-        name:"41",
-        color:null,
-        image:null,
-        thumb:null,
-        title:"41"
-        },
-        ]
-        }
-    ]
-};
-
-
-const initialState = {
-    isLoading: false,
-    isFetched: false,
-    error: false,
-    id: null,
-    title: "Product title",
-    discountedPrice: 25,
-    oldPrice: 30,
-    discountedPercentage: 30,
-    productDetails: {
-        mainImage: "",
-        gallery: [],
-        ratingCount: null,
-        ratingAverage: null,
-        skus: [],
-        variants: []
-    },
-    ui: {
-        swiper: null,
-        isVariantColorSelected: false,
-        selectedVariantColor: {
-          color: [],
-          size: []
-        },
-    }
-};
-
 
 export default product;
